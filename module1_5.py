7210c5d16335b024b156ae4d0c4785d4108db22f
orshanskayasofia <orshanskayasofia@gmail.com>
2024-08-22 17:32:16 +0300

    Add files via upload

---
+++ module1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1 , 2.5 , 'Plato' , True)
+print(immutable_var)
+#immutable_var[0] = 5
+# #элементы в кортеже неизменяемые
+mytable_list = [2, 5.5 ,'Socrates' , False]
+mytable_list[2] = 'Aristotle'
+print(mytable_list)
